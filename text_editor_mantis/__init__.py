c2729007c702f0be7d7901417f24ce282b0f6a6a
zeffii <tetha.z@gmail.com>
2014-05-25 13:19:15 +0200

    this repo is for junk and random development

---
+++ text_editor_mantis/__init__.py
@@ -0,0 +1,50 @@
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you may redistribute it, and/or
+# modify it, under the terms of the GNU General Public License
+# as published by the Free Software Foundation - either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, write to:
+#
+#	the Free Software Foundation Inc.
+#	51 Franklin Street, Fifth Floor
+#	Boston, MA 02110-1301, USA
+#
+# or go online at: http://www.gnu.org/licenses/ to view license options.
+#
+# ***** END GPL LICENCE BLOCK *****
+
+bl_info = {
+    "name": "Scrubbing Mantis",
+    "author": "Dealga McArdle",
+    "version": (0, 1, 0),
+    "blender": (2, 6, 4),
+    "location": "TextEditor + toolbar",
+    "description": "helps quick re-eval of code, during slider updates",
+    "wiki_url": "",
+    "tracker_url": "",
+    "category": "Text Editor"}
+
+
+if "bpy" in locals():
+    import imp
+    imp.reload(mantis)
+else:
+    from . import mantis
+
+import bpy
+
+
+def register():
+    bpy.utils.register_module(__name__)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
---
+++ text_editor_mantis/mantis.py
@@ -0,0 +1,69 @@
+# ***** BEGIN GPL LICENSE BLOCK *****
+#
+# This program is free software; you may redistribute it, and/or
+# modify it, under the terms of the GNU General Public License
+# as published by the Free Software Foundation - either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, write to:
+#
+#   the Free Software Foundation Inc.
+#   51 Franklin Street, Fifth Floor
+#   Boston, MA 02110-1301, USA
+#
+# or go online at: http://www.gnu.org/licenses/ to view license options.
+#
+# ***** END GPL LICENCE BLOCK *****
+
+''' 
+roadmap and thoughts:
+https://github.com/zeffii/bpy_script/issues/2 
+'''
+
+import bpy
+
+
+# class GistDownloadButton(bpy.types.Operator):
+#     """Defines a button"""
+#     bl_idname = "scene.download_gist"
+#     bl_label = "Download given gist from id only"
+ 
+#     def execute(self, context):
+#         # could name this filename instead of gist_id for new .blend text.
+#         gist_id = context.scene.gist_id_property
+#         bpy.data.texts.new(gist_id)
+#         bpy.data.texts[gist_id].write(get_file(gist_id))
+#         return{'FINISHED'}
+
+
+class MantisPropertiesPanel(bpy.types.Panel):
+    """Creates a Panel in the TextEditor properties window"""
+    bl_label = "Mantis replcv"
+    bl_idname = "TEXT_OT_somefunction"
+    bl_space_type = "TEXT_EDITOR"
+    bl_region_type = "UI"
+    bl_context = "object"
+ 
+    def draw(self, context):
+        layout = self.layout
+        row = layout.row()
+        scn = bpy.context.scene
+
+        layout.label("rwarrrr")
+        # display stringbox and download button
+        #self.layout.prop(scn, "gist_id_property")
+        #self.layout.operator("scene.download_gist", text='Download to .blend')
+
+
+def register():
+    bpy.utils.register_class(MantisPropertiesPanel)
+
+
+def unregister():
+    bpy.utils.unregister_class(MantisPropertiesPanel)
